4ee5a1add2ff31904528be344fcb0ee076e48fe1
Juan Diego Naranjo <JuanNaranjo1206@gmail.com>
2019-09-15 18:32:35 -0500

    controller added

---
+++ src/ui/MinecraftStorageController.java
@@ -0,0 +1,332 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class MinecraftStorageController {
+
+    @FXML
+    private Button addInventary;
+
+    @FXML
+    private Button addQuickAccess;
+
+    @FXML
+    private Label labelNumberQuickAccess;
+
+    @FXML
+    private Button nextQuickAccess;
+
+    @FXML
+    private ImageView block11;
+
+    @FXML
+    private ImageView block12;
+
+    @FXML
+    private ImageView block13;
+
+    @FXML
+    private ImageView block14;
+
+    @FXML
+    private ImageView block15;
+
+    @FXML
+    private ImageView block16;
+
+    @FXML
+    private ImageView block17;
+
+    @FXML
+    private ImageView block18;
+
+    @FXML
+    private ImageView block19;
+
+    @FXML
+    private ImageView block21;
+
+    @FXML
+    private ImageView block22;
+
+    @FXML
+    private ImageView block23;
+
+    @FXML
+    private ImageView block24;
+
+    @FXML
+    private ImageView block25;
+
+    @FXML
+    private ImageView block26;
+
+    @FXML
+    private ImageView block27;
+
+    @FXML
+    private ImageView block28;
+
+    @FXML
+    private ImageView block29;
+
+    @FXML
+    private ImageView block31;
+
+    @FXML
+    private ImageView block32;
+
+    @FXML
+    private ImageView block33;
+
+    @FXML
+    private ImageView block34;
+
+    @FXML
+    private ImageView block35;
+
+    @FXML
+    private ImageView block36;
+
+    @FXML
+    private ImageView block37;
+
+    @FXML
+    private ImageView block38;
+
+    @FXML
+    private ImageView block39;
+
+    @FXML
+    private ImageView block41;
+
+    @FXML
+    private ImageView block42;
+
+    @FXML
+    private ImageView block43;
+
+    @FXML
+    private ImageView block44;
+
+    @FXML
+    private ImageView block45;
+
+    @FXML
+    private ImageView block46;
+
+    @FXML
+    private ImageView block47;
+
+    @FXML
+    private ImageView block48;
+
+    @FXML
+    private ImageView block49;
+
+    @FXML
+    private ImageView number11;
+
+    @FXML
+    private ImageView number12;
+
+    @FXML
+    private ImageView number13;
+
+    @FXML
+    private ImageView number14;
+
+    @FXML
+    private ImageView number15;
+
+    @FXML
+    private ImageView number16;
+
+    @FXML
+    private ImageView number17;
+
+    @FXML
+    private ImageView number18;
+
+    @FXML
+    private ImageView number19;
+
+    @FXML
+    private ImageView number21;
+
+    @FXML
+    private ImageView number22;
+
+    @FXML
+    private ImageView number23;
+
+    @FXML
+    private ImageView number24;
+
+    @FXML
+    private ImageView number25;
+
+    @FXML
+    private ImageView number26;
+
+    @FXML
+    private ImageView number27;
+
+    @FXML
+    private ImageView number28;
+
+    @FXML
+    private ImageView number29;
+
+    @FXML
+    private ImageView number31;
+
+    @FXML
+    private ImageView number32;
+
+    @FXML
+    private ImageView number33;
+
+    @FXML
+    private ImageView number34;
+
+    @FXML
+    private ImageView number35;
+
+    @FXML
+    private ImageView number36;
+
+    @FXML
+    private ImageView number37;
+
+    @FXML
+    private ImageView number38;
+
+    @FXML
+    private ImageView number39;
+
+    @FXML
+    private ImageView number41;
+
+    @FXML
+    private ImageView number42;
+
+    @FXML
+    private ImageView number43;
+
+    @FXML
+    private ImageView number44;
+
+    @FXML
+    private ImageView number45;
+
+    @FXML
+    private ImageView number46;
+
+    @FXML
+    private ImageView number47;
+
+    @FXML
+    private ImageView number48;
+
+    @FXML
+    private ImageView number49;
+
+    @FXML
+    private ImageView block51;
+
+    @FXML
+    private ImageView number51;
+
+    @FXML
+    private ImageView block52;
+
+    @FXML
+    private ImageView number52;
+
+    @FXML
+    private ImageView block53;
+
+    @FXML
+    private ImageView number53;
+
+    @FXML
+    private ImageView block54;
+
+    @FXML
+    private ImageView number54;
+
+    @FXML
+    private ImageView block55;
+
+    @FXML
+    private ImageView number55;
+
+    @FXML
+    private ImageView block56;
+
+    @FXML
+    private ImageView number56;
+
+    @FXML
+    private ImageView block57;
+
+    @FXML
+    private ImageView number57;
+
+    @FXML
+    private ImageView block58;
+
+    @FXML
+    private ImageView number58;
+
+    @FXML
+    private ImageView block59;
+
+    @FXML
+    private ImageView number59;
+
+    @FXML
+    private Button generateBlock;
+
+    @FXML
+    private Button quickAccessFeature;
+
+    @FXML
+    private Button buildingFeature;
+
+    @FXML
+    void addBlockToInventary(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addBlockToQuickAccessBar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void generateBlocks(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nextQuickAccessBar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showBuildingBar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showQuickAccessBar(ActionEvent event) {
+
+    }
+
+}
